2fe630fefb4de93c2a85252094b4b49a019e6355
Eduardo Simioni <edusimioni@yahoo.com.br>
2018-07-27 11:25:58 -0300

    Add seed Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+lime = Ingredient.create!(name: 'lime')
+ice = Ingredient.create!(name: 'ice')
+pinga = Ingredient.create!(name: 'pinga')
+rum = Ingredient.create!(name: 'rum')
+mint = Ingredient.create!(name: 'mint leaves')
+sugar = Ingredient.create!(name: 'sugar')
+
+caipirinha = Cocktail.create!(name: 'caipirinha')
+mojito = Cocktail.create!(name: 'mojito')
+
+Dose.create!(cocktail: caipirinha, ingredient: lime, description: 'limão')
+Dose.create!(cocktail: caipirinha, ingredient: ice, description: 'gelo')
+Dose.create!(cocktail: caipirinha, ingredient: pinga, description: 'pinga da boa')
+Dose.create!(cocktail: caipirinha, ingredient: sugar, description: 'açúcar')
+Dose.create!(cocktail: mojito, ingredient: lime, description: 'limão')
+Dose.create!(cocktail: mojito, ingredient: ice, description: 'gelo')
+Dose.create!(cocktail: mojito, ingredient: rum, description: 'rum do pirata')
+Dose.create!(cocktail: mojito, ingredient: sugar, description: 'açúcar')
+Dose.create!(cocktail: mojito, ingredient: mint, description: 'folhinha de hortelã')
